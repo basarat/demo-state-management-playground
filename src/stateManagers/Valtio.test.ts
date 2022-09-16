05964723c8bf357aa65be6db0c98f0a79b16e2e4
Basarat <basaratali@gmail.com>
2022-09-16 15:24:04 +1000

    async

--- src/stateManagers/Valtio.test.ts
+++ src/stateManagers/Valtio.test.ts
@@ -1,7 +1,7 @@
 import { counters, addCounter } from './Valtio';
 
-test('Valtio', () => {
+test('Valtio', async () => {
   expect(counters.length).toBe(0);
-  addCounter();
+  await addCounter();
   expect(counters.length).toBe(1);
 });
